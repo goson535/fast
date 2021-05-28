9b9add7233b4cc8593fbd991affb1b3bc364b892
goson535 <84283668+goson535@users.noreply.github.com>
2021-05-28 10:51:22 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'admin_faazur' );
+define( 'DB_NAME', 'admin_faazur3' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'admin_faazur' );
+define( 'DB_USER', 'admin_faazur3' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', 'Hk8ecT0IyH' );
+define( 'DB_PASSWORD', 'Hk8ecT0IyH3' );
 
 /** Имя сервера MySQL */
 define( 'DB_HOST', '92.119.113.153:3306' );
